5aa00bf43a4bdcac0c95eef26528538a5297fc8e
Kevin <kelvinian87@gmail.com>
2022-11-18 22:30:16 +0300

    sign up fragment design complete

---
+++ app/src/main/java/com/kev/noted/view/LoginFragment.kt
@@ -0,0 +1,8 @@
+package com.kev.noted.view
+
+import androidx.fragment.app.Fragment
+import com.kev.noted.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginFragment : Fragment(R.layout.fragment_login)
---
+++ app/src/main/java/com/kev/noted/view/SignUpFragment.kt
@@ -0,0 +1,8 @@
+package com.kev.noted.view
+
+import androidx.fragment.app.Fragment
+import com.kev.noted.R
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class SignUpFragment:Fragment(R.layout.fragment_sign_up)
