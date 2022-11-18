d04eae0c5bc770b59eb5c6805cd621ef8cdca7d6
Kevin <kelvinian87@gmail.com>
2022-11-19 00:36:47 +0300

    users are able to create an account succesfully

--- app/src/main/java/com/kev/noted/view/fragments/SignUpFragment.kt
+++ app/src/main/java/com/kev/noted/view/fragments/SignUpFragment.kt
@@ -8,18 +8,19 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
+import androidx.navigation.fragment.findNavController
 import com.kev.noted.R
 import com.kev.noted.databinding.FragmentSignUpBinding
 import com.kev.noted.viewmodel.AuthViewModel
+import com.kev.util.LoadingState
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SignUpFragment:Fragment(R.layout.fragment_sign_up){
-	private var _binding : FragmentSignUpBinding? = null
+class SignUpFragment : Fragment(R.layout.fragment_sign_up) {
+	private var _binding: FragmentSignUpBinding? = null
 	private val binding get() = _binding!!
 
-	private val viewModel : AuthViewModel by viewModels()
+	private val viewModel: AuthViewModel by viewModels()
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
@@ -42,23 +43,51 @@ class SignUpFragment:Fragment(R.layout.fragment_sign_up){
 		val password = binding.userPasswordEtv.text.toString()
 		val confirmPassword = binding.confirmPasswordEtv.text.toString()
 
-		if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-			Toast.makeText(requireContext(), "Ensure email is correct", Toast.LENGTH_SHORT).show()
-		}
-		else if(name.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()){
-			Toast.makeText(requireContext(), "Ensure all fields are filled", Toast.LENGTH_SHORT).show()
-		}
-		else if(password ==confirmPassword && confirmPassword.length <=  5){
-		Toast.makeText(requireContext(), "Password must contain 5 or more characters", Toast.LENGTH_SHORT).show()
+
+		if (name.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+			Toast.makeText(requireContext(), "Ensure all fields are filled.", Toast.LENGTH_SHORT)
+				.show()
+		} else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+			Toast.makeText(
+				requireContext(),
+				"Ensure email is correctly typed. ",
+				Toast.LENGTH_SHORT
+			)
+				.show()
+		} else if (password == confirmPassword && confirmPassword.length <= 5) {
+			Toast.makeText(
+				requireContext(),
+				"Password must contain 5 or more characters",
+				Toast.LENGTH_SHORT
+			).show()
+
+		} else if (password != confirmPassword) {
+
+			Toast.makeText(requireContext(), "Passwords don't match", Toast.LENGTH_SHORT).show()
+		} else {
+			viewModel.signUp(name, email, password)
 
 		}
-		else if(password != confirmPassword){
-			binding.confirmPasswordEtv.error = "Passwords do not match"
-		}
 
-		else{
-		/*viewModel.signUp(name, email, password)*/
-			Toast.makeText(requireContext(), "Conditions yote sawa", Toast.LENGTH_SHORT).show()
+
+		viewModel.signUpLiveData.observe(viewLifecycleOwner) { state ->
+
+			when (state) {
+				is LoadingState.Loading -> {
+					binding.progressBar.visibility = View.VISIBLE
+				}
+
+				is LoadingState.Error -> {
+					binding.progressBar.visibility = View.GONE
+					Toast.makeText(requireContext(), state.message, Toast.LENGTH_SHORT).show()
+				}
+
+				is LoadingState.Success -> {
+					binding.progressBar.visibility = View.GONE
+					findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+				}
+			}
+
 		}
 
 
